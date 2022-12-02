8711512a08f6757fbd0af0bc2f95c4dcf58ae127
DennsHartstock <dennis.hartstock@web.de>
2022-12-02 10:30:12 +0100

    refactor

--- app/src/main/java/com/example/dogs/MainActivity.java
+++ app/src/main/java/com/example/dogs/MainActivity.java
@@ -4,53 +4,23 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import androidx.lifecycle.ViewModelProvider;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String BASE_URL = "https://dog.ceo/api/breeds/image/random";
+    public static final String TAG = "MainActivity";
+
+    private MainViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        loadDogImage();
-    }
-
-    private void loadDogImage() {
-        new Thread(() -> {
-            try {
-                URL url = new URL(BASE_URL);
-                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-                InputStream inputStream = httpURLConnection.getInputStream();
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
-                StringBuilder data = new StringBuilder();
-                String result;
-                do {
-                    result = bufferedReader.readLine();
-                    if (result != null) {
-                        data.append(result);
-                    }
-                } while (result != null);
-
-                JSONObject jsonObject = new JSONObject(data.toString());
-                String message = jsonObject.getString("message");
-                String status = jsonObject.getString("status");
-                DogImage dogImage = new DogImage(message, status);
-
-                Log.d("MainActivity", dogImage.toString());
-            } catch (Exception e) {
-                Log.d("MainActivity", e.toString());
-            }
-        }).start();
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        viewModel.loadDogImage();
+        viewModel.getDogImageMutableLiveData().observe(
+                this,
+                dogImage -> Log.d(TAG, dogImage.toString())
+        );
     }
 }
---
+++ app/src/main/java/com/example/dogs/MainViewModel.java
@@ -0,0 +1,87 @@
+package com.example.dogs;
+
+import android.app.Application;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Single;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+public class MainViewModel extends AndroidViewModel {
+
+    public static final String BASE_URL = "https://dog.ceo/api/breeds/image/random";
+    public static final String KEY_MESSAGE = "message";
+    public static final String KEY_STATUS = "status";
+    public static final String TAG = "MainViewModel";
+
+    private final MutableLiveData<DogImage> dogImageMutableLiveData = new MutableLiveData<>();
+
+    private final CompositeDisposable compositeDisposable = new CompositeDisposable();
+
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public LiveData<DogImage> getDogImageMutableLiveData() {
+        return dogImageMutableLiveData;
+    }
+
+    public void loadDogImage() {
+        Disposable disposable = loadDogImageRx()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        dogImageMutableLiveData::setValue,
+                        throwable -> Log.d(TAG, "Error: " + throwable.getMessage())
+                );
+        compositeDisposable.add(disposable);
+    }
+
+    private Single<DogImage> loadDogImageRx() {
+        return Single.fromCallable(() -> {
+            URL url = new URL(BASE_URL);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            InputStream inputStream = httpURLConnection.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            StringBuilder data = new StringBuilder();
+            String result;
+            do {
+                result = bufferedReader.readLine();
+                if (result != null) {
+                    data.append(result);
+                }
+            } while (result != null);
+
+            JSONObject jsonObject = new JSONObject(data.toString());
+            String message = jsonObject.getString(KEY_MESSAGE);
+            String status = jsonObject.getString(KEY_STATUS);
+            return new DogImage(message, status);
+        });
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        compositeDisposable.dispose();
+    }
+}
+
+
+
