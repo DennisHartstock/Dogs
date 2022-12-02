a540b4123ab133869a695fe26b0cc2e2b93f0676
DennsHartstock <dennis.hartstock@web.de>
2022-12-02 09:01:30 +0100

    add data loading

---
+++ app/src/main/java/com/example/dogs/DogImage.java
@@ -0,0 +1,20 @@
+package com.example.dogs;
+
+public class DogImage {
+
+    private final String message;
+    private final String status;
+
+    public DogImage(String message, String status) {
+        this.message = message;
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
