268fc412b4486a7143069cd94c0ed36d735f21ac
DennsHartstock <dennis.hartstock@web.de>
2022-12-04 09:12:02 +0100

    implement Retrofit

--- app/src/main/java/com/example/dogs/MainViewModel.java
+++ app/src/main/java/com/example/dogs/MainViewModel.java
@@ -7,14 +7,6 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.core.Single;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
@@ -22,10 +14,6 @@ import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainViewModel extends AndroidViewModel {
-
-    public static final String BASE_URL = "https://dog.ceo/api/breeds/image/random";
-    public static final String KEY_MESSAGE = "message";
-    public static final String KEY_STATUS = "status";
 
     private final MutableLiveData<DogImage> dogImageMutableLiveData = new MutableLiveData<>();
     private final MutableLiveData<Boolean> isLoading = new MutableLiveData<>();
@@ -63,27 +51,7 @@ public class MainViewModel extends AndroidViewModel {
     }
 
     private Single<DogImage> loadDogImageRx() {
-        return Single.fromCallable(() -> {
-            URL url = new URL(BASE_URL);
-            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-            InputStream inputStream = httpURLConnection.getInputStream();
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
-            StringBuilder data = new StringBuilder();
-            String result;
-            do {
-                result = bufferedReader.readLine();
-                if (result != null) {
-                    data.append(result);
-                }
-            } while (result != null);
-
-            JSONObject jsonObject = new JSONObject(data.toString());
-            String message = jsonObject.getString(KEY_MESSAGE);
-            String status = jsonObject.getString(KEY_STATUS);
-            return new DogImage(message, status);
-        });
+        return ApiFactory.getApiService().loadDogImage();
     }
 
     @Override
